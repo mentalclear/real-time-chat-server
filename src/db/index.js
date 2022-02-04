cd6e05d42b76c42155564e3dc8fa75110e9cda42
Dmitrii Kilishek <mentalclear@gmail.com>
2022-02-04 09:10:20 -0500

    Continue with 6.7

--- src/db/index.js
+++ src/db/index.js
@@ -1,2 +1,4 @@
 export { db } from './db';
+export { createConversation } from './createConversation';
+export { getAllUsers } from './getAllUsers';
 export { getUserConversations } from './getUserConversations';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
+import { createConversationRoute } from './createConversationRoute';
+import { getAllUsersRoute } from './getAllUsersRoute';
 import { getUserConversationsRoute } from './getUserConversationsRoute';
+
 
 export { protectRouteMiddleware } from './protectRouteMiddleware';
 export const routes = [ 
-    getUserConversationsRoute,
+    createConversationRoute,
+    getAllUsersRoute,
+    getUserConversationsRoute,    
 ];
