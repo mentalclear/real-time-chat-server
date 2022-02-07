361fa3d0a49916c08b941e636fef55f574c7e1c2
Dmitrii Kilishek <mentalclear@gmail.com>
2022-02-07 07:48:08 -0500

    Continue 6.7 from 15:50

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
+import http from 'http';
 import express from "express";
 import { routes, protectRouteMiddleware } from "./routes";
 import * as admin from 'firebase-admin';
+import socketIo from 'socket.io';
 import credentials from './credentials.json';
 import { db } from './db';
 import bodyParser from "body-parser";
@@ -18,9 +20,27 @@ routes.forEach(route => {
     app[route.method](route.path, protectRouteMiddleware, route.handler);
 });
 
+const server = http.createServer(app);
+// Added CORS here since it didn't work otherwise.
+const io = socketIo(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+});
+
+io.on('connection', async socket => {
+    console.log("A new client has connected to socket.io");
+    console.log(socket.handshake.query.conversationId);
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
 const start = async () => {
     await db.connect('mongodb://localhost:27017');
-    app.listen(8080,() => {
+    server.listen(8080,() => {
         console.log('Server is listening on port 8080');
     });
 }
